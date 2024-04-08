293041f286000f8ad6f450bda58bcc22994a6a4f
Joaquin Rivera <u202110072@upc.edu.pe>
2024-04-07 20:45:10 -0500

    damn

--- src/main/java/pe/edu/upc/abilityhelpv1/Abilityhelpv1Application.java
+++ src/main/java/pe/edu/upc/abilityhelpv1/Abilityhelpv1Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Abilityhelpv1Application { //xdxdxdxd
+public class Abilityhelpv1Application { //xdxdxdxdxdxdxdxd
 
 	public static void main(String[] args) {
 		SpringApplication.run(Abilityhelpv1Application.class, args);
