06aa9c1fcbe4c95586715ef8e87614e7446d6e16
Joaquin Rivera <u202110072@upc.edu.pe>
2024-04-10 18:44:42 -0500

    deleted meetings, update scheduleuser

--- src/main/java/pe/edu/upc/abilityhelpv1/entities/ScheduleUser.java
+++ src/main/java/pe/edu/upc/abilityhelpv1/entities/ScheduleUser.java
@@ -1,5 +1,60 @@
 package pe.edu.upc.abilityhelpv1.entities;
 
-public class ScheduleUser {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name="ScheduleUser")
+public class ScheduleUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUserSchedule;
+
+    @ManyToOne
+    @JoinColumn(name="userId")
+    private User idUser;
+
+    @ManyToOne
+    @JoinColumn(name="scheduleId")
+    private Schedule iSchedule;
+
+    public ScheduleUser() {
+    }
+
+    public ScheduleUser(int idUserSchedule, User idUser, Schedule iSchedule) {
+        this.idUserSchedule = idUserSchedule;
+        this.idUser = idUser;
+        this.iSchedule = iSchedule;
+    }
+
+    public int getIdUserSchedule() {
+        return idUserSchedule;
+    }
+
+    public void setIdUserSchedule(int idUserSchedule) {
+        this.idUserSchedule = idUserSchedule;
+    }
+
+    public User getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(User idUser) {
+        this.idUser = idUser;
+    }
+
+    public Schedule getiSchedule() {
+        return iSchedule;
+    }
+
+    public void setiSchedule(Schedule iSchedule) {
+        this.iSchedule = iSchedule;
+    }
+
+    
 }
