e0123d84a2b3baea0a841a659566ef7a68dd7549
Joaquin Rivera <u202110072@upc.edu.pe>
2024-04-10 18:37:23 -0500

    update meetings

--- src/main/java/pe/edu/upc/abilityhelpv1/entities/Meetings.java
+++ src/main/java/pe/edu/upc/abilityhelpv1/entities/Meetings.java
@@ -1,5 +1,29 @@
 package pe.edu.upc.abilityhelpv1.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Meetings")
 public class Meetings {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idMeet;
+
+    @ManyToOne
+    @JoinColumn(name = "userIdschedule")
+    private User idUserSchedule;
+
+    @ManyToOne
+    @JoinColumn(name = "coachId")
+    private User idCoach;
+
+    
 
 }
