f4364fe089b5ad8628816869f32bc5a66767ef0a
Joaquin Rivera <joaquinrivram@arizona.edu>
2024-04-27 12:41:36 -0500

    update rol

---
+++ src/main/java/pe/edu/upc/abilityhelpv1/entities/Rol.java
@@ -0,0 +1,55 @@
+package pe.edu.upc.abilityhelpv1.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "Rol", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "rol"})})
+public class Rol implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idRol;
+    @Column(name = "rol", nullable = false, length = 35)
+    private String rol;
+
+    //agregar el one to many para saber que rol esta asignado a cada usuario
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Rol() {
+    }
+
+    public Rol(Long idRol, String rol, User user) {
+        this.idRol = idRol;
+        this.rol = rol;
+        this.user = user;
+    }
+
+    public Long getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(Long idRol) {
+        this.idRol = idRol;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
