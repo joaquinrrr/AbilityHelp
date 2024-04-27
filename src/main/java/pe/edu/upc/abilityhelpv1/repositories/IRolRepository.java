be7e9ffc1de6b63247fe1d345be822942d3c1a00
albertofran <albertofran43@gmail.com>
2024-04-27 16:20:47 -0500

    push

--- src/main/java/pe/edu/upc/abilityhelpv1/repositories/IRolRepository.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.abilityhelpv1.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.abilityhelpv1.entities.Rol;
-
-public interface IRolRepository extends JpaRepository<Rol,Integer> {
-}
