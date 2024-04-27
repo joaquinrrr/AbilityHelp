b088140452a925522c207ebd621c49d97701c2f7
Joaquin Rivera <86880893+joaquinrrr@users.noreply.github.com>
2024-04-27 12:05:58 -0500

    Delete src directory

--- src/main/java/pe/edu/upc/abilityhelpv1/servicesinterfaces/IAdminServices.java
+++
@@ -1,7 +0,0 @@
-package pe.edu.upc.abilityhelpv1.servicesinterfaces;
-import pe.edu.upc.abilityhelpv1.entities.Admin;
-import java.util.List;
-public interface IAdminServices {
-    public void insert(Admin admin);
-    public List<Admin> list();
-}
