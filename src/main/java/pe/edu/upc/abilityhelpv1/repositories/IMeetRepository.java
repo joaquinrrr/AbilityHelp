edc6f05149b232d6e521bf55a812390f60809fe0
Joaquin Rivera <joaquinrivram@arizona.edu>
2024-04-27 18:49:23 -0500

    commit

--- src/main/java/pe/edu/upc/abilityhelpv1/repositories/IMeetRepository.java
+++ src/main/java/pe/edu/upc/abilityhelpv1/repositories/IMeetRepository.java
@@ -18,16 +18,16 @@ public interface IMeetRepository extends JpaRepository<Meeting, Integer> {
     public List<String[]> QuantityMeetPerCoach(@Param("name") String name);
 
     @Query(value = "select us.username as Name, sc.week_day as Date , sc.start_hour, sc.finish_hour  from meeting as me \n" +
-            "inner join user_table as us ON us.id_user = me.user_id \n" +
+            "inner join user_table as us ON us.id_user = me.student_id \n" +
             "inner join schedule as sc ON sc.id_schedule = me.id_schedule \n" +
             "where sc.week_day =:date \n" +
             "group by us.username, sc.week_day, sc.start_hour, sc.finish_hour", nativeQuery = true)
     public List<String[]> meetingPerUserDate(@Param("date") LocalDate date);
 
-    @Query(value = "SELECT to_char(s.week_day, 'Month') AS mes,\n" +
-            "COUNT(*) AS cantidad_reuniones\n" +
-            "FROM Meeting m\n" +
-            "INNER JOIN Schedule s ON m.id_schedule = s.id_schedule\n" +
+    @Query(value = "SELECT to_char(s.week_day, 'Month') AS mes, \n" +
+            "COUNT(*) AS cantidad_reuniones \n" +
+            "FROM Meeting m \n" +
+            "INNER JOIN Schedule s ON m.id_schedule = s.id_schedule \n" +
             "WHERE date_part('year', s.week_day) =:year \n" +
             "GROUP BY mes", nativeQuery = true)
     public List<String[]> quantityMeetsPerMonth(@Param("year") int year);
