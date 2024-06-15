a222fd35221ce2b07f088ff21a957dffed85e100
Joaquin Rivera <joaquinrivram@arizona.edu>
2024-06-15 12:06:00 -0500

    backend update

---
+++ src/main/java/pe/edu/upc/abilityhelpv1/util/CorsConfig.java
@@ -0,0 +1,24 @@
+package pe.edu.upc.abilityhelpv1.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:4200")  // Ajusta el origen permitido
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
